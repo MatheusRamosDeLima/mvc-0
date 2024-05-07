60498f390171f0f9f37652b76ab86e1bd4f23d75
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-05-06 23:29:19 -0300

    Added tests in isError404 to verify another errors

--- app/Core/App.php
+++ app/Core/App.php
@@ -44,6 +44,12 @@ class App {
     }
     private function isError404(string $controller, string $method, array $params) : void {
         $controllerPath = __DIR__."/../Controllers/$controller.php";
+        echo "<h2>Test in isError404!</h2>";
+        echo !file_exists($controllerPath) ? "<p>Controller file doesn't exist</p>" : "<p>Controller file exists</p>";
+        echo !method_exists($controller, $method) ? "<p>Method $method doesn't exist in $controller</p>" : "<p>Method $method exists in $controller</p>";
+        echo method_exists('Controller', $method) ? "<p>Method $method exists in Controller</p>" : "<p>Method $method doesn't exist in Controller</p>";
+        echo !$this->isParamsValid($controller, $method, $params) ? "<p>The params aren't valid</p>" : "<p>The params are valid</p>";
+        echo $method === '__construct' ? "<p>The method is construct</p>" : "<p>The method isn't construct</p>";
         if (!file_exists($controllerPath) || !method_exists($controller, $method) || method_exists('Controller', $method) || !$this->isParamsValid($controller, $method, $params) || $method === '__construct') {
             $this->controller = 'ErrorController';
             $this->method = 'index';
